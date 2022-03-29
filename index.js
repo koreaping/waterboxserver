e595e1a7729e103666b6c041d453f4b9400b009b
tmdrb-lab <tmdrb8094854@naver.com>
2022-03-30 02:10:18 +0900

    modify

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ const serial = new SerialPort({
     path : '/dev/ttyUSB0',
     baudRate : 115200
 });
-process.setMaxListeners(50);
+process.setMaxListeners(5);
 
 var app = express();
 var port = 9000 
 
 var Heater = false;
-var H2o = false;
+var H2o = true;
 
 const {Gpio} = require('onoff');
 const channel2 = new Gpio('17' , 'out');
 const channel1 = new Gpio('18' ,'out');
-
-setInterval(()=> {
-    channel2.writeSync(Heater ? 0 : 1);
-    channel1.writeSync(H2o ? 0 : 1);
-    Heater = !Heater
-    H2o = !H2o
-}, 10000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}))
@@ -34,10 +27,13 @@ const server = app.listen(port,() => {
 const io = require('socket.io')(server);
 
 io.on('connection' , (socket) =>{
+
     console.log("Connected Successfully " , socket.id);
 
     socket.on('disconnect' , ()=>{
         console.log("Disconnected ", socket.id);
+        socket.disconnect();
+        serial.removeAllListeners();
     });
 
     serial.on('data' , function(data) {
@@ -45,7 +41,16 @@ io.on('connection' , (socket) =>{
         console.log(dataList)
         socket.emit("RTD" , dataList[0]);
         socket.emit("PH" , dataList[1]);
-        socket.emit("DO" ,"29.5");
+        socket.emit("DO" ,dataList[2]);
+        var temp = parseFloat(dataList[0]);
+        if(temp > 27){
+            Heater = false;
+        }else{
+            Heater = true;
+        }
+        channel2.writeSync(Heater ? 1 : 0);
+        channel1.writeSync(H2o ? 1 : 0);
+    
         if(Heater){
             socket.emit("Heater" , "true");
         }
